3752ee837b259a3030af7b0f055badcb32e21a49
neerajdask <iamneerajdas@gmail.com>
2021-08-12 12:35:38 +0530

    Added v2

--- src/app/drawer/v1/v1.component.ts
+++ src/app/drawer/v1/v1.component.ts
@@ -3,7 +3,6 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 @Component({
   selector: 'app-v1',
   templateUrl: './v1.component.html',
-  styleUrls: ['./v1.component.scss'],
 })
 export class V1Component implements OnInit {
   @Input() isOpen = false;
