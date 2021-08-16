c5a903f9480783fad535df23126d0a1760781167
neerajdask <iamneerajdas@gmail.com>
2021-08-16 12:31:28 +0530

    v5 Completed

--- src/app/drawer/v5/v5-drawer/v5-drawer.component.ts
+++ src/app/drawer/v5/v5-drawer/v5-drawer.component.ts
@@ -3,6 +3,7 @@ import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-v5-drawer',
   templateUrl: './v5-drawer.component.html',
+  styleUrls: ['./v5-drawer.component.scss'],
   exportAs: 'drawerComp'
 })
 export class V5DrawerComponent implements OnInit {
@@ -16,21 +17,21 @@ export class V5DrawerComponent implements OnInit {
 
   ngOnInit(): void {}
 
-  get drawerStyles() {
-    const commonStyles = { width: `${this.width}px` };
+  // get drawerStyles() {
+  //   const commonStyles = { width: `${this.width}px` };
 
-    if (this.position === 'right') {
-      return {
-        ...commonStyles,
-        right: `${this.isOpen ? 0 : -1 * this.width}px`,
-      };
-    } else {
-      return {
-        ...commonStyles,
-        left: `${this.isOpen ? 0 : -1 * this.width}px`,
-      };
-    }
-  }
+  //   if (this.position === 'right') {
+  //     return {
+  //       ...commonStyles,
+  //       right: `${this.isOpen ? 0 : -1 * this.width}px`,
+  //     };
+  //   } else {
+  //     return {
+  //       ...commonStyles,
+  //       left: `${this.isOpen ? 0 : -1 * this.width}px`,
+  //     };
+  //   }
+  // }
 
   close() {
     this.closeDrawer.emit();
